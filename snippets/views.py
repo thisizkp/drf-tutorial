24a16fa4472dee51c636974aef5b0da15578ee43
Prasanth Karri <prasanth.25k@gmail.com>
2020-02-15 16:16:11 +0530

    use router

--- snippets/views.py
+++ snippets/views.py
@@ -1,20 +1,10 @@
-from django.http import Http404
 from django.contrib.auth.models import User
-from rest_framework import status, generics, permissions, renderers, viewsets
+from rest_framework import permissions, renderers, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework.decorators import api_view, action
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
