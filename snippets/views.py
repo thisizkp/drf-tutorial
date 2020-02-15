629ffe49ea3dc3df607747008d57cc600edf35c2
Prasanth Karri <prasanth.25k@gmail.com>
2020-02-15 14:31:08 +0530

    add root url

--- snippets/views.py
+++ snippets/views.py
@@ -2,9 +2,19 @@ from django.http import Http404
 from django.contrib.auth.models import User
 from rest_framework import status, generics, permissions
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
